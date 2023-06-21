1cc080f92d80b018a48e3232c5bf22be74907ed3
BuildTools <unconfigured@null.spigotmc.org>
2023-06-21 02:21:28 -0400

    changes

--- src/main/java/net/iamaprogrammer/customworldicons/mixin/GameRendererMixin.java
+++ src/main/java/net/iamaprogrammer/customworldicons/mixin/GameRendererMixin.java
@@ -12,7 +12,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -32,10 +34,11 @@ public abstract class GameRendererMixin {
 
     @Shadow @Final private static Logger LOGGER;
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;updateWorldIcon()V"))
-    private void injected(GameRenderer instance) {
+    @Inject(method = "Lnet/minecraft/client/render/GameRenderer;updateWorldIcon()V", at = @At(value = "HEAD"), cancellable = true)
+    private void injected(CallbackInfo ci) {
         updateIcon(WorldIconScreen.SELECTED_ICON, WorldIconScreen.SELECTED_ICON != null);
         WorldIconScreen.SELECTED_ICON = null;
+        ci.cancel();
     }
 
     public void updateIcon(String iconPath, boolean isCustom) {
